cc9722c5ff10568f3a103d8e65f91af636fd33e5
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-01-22 10:48:39 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#inlcude<conio.h>
+int main
+{
+int a;
+if(a>0)
+printf("the given number is positive number");
+else if(a<0)
+printf("the given number is negative number");
+else
+pirntf("the number is zero");
+return 0;
+}
